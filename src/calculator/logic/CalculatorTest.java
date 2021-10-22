c87c7187029007f44b49d23e5169add5d0f83ce7
Your Name <you@example.com>
2021-10-22 17:45:34 +0200

    Initial commit

---
+++ src/calculator/logic/CalculatorTest.java
@@ -0,0 +1,104 @@
+package calculator.logic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import calculator.Calculator;
+import calculator.CalculatorException;
+import calculator.Calculator.Operation;
+import calculator.logic.CalculatorImpl;
+
+public class CalculatorTest {
+
+	@Test
+	public void testSimpleModOperation() throws Exception {
+
+		//setup
+		Calculator calc = new CalculatorImpl();
+		
+		//execute
+		calc.push(14);
+		calc.push(3);
+		double result = calc.perform(Operation.mod);
+
+		//verify
+		assertEquals(2, result, 0);
+
+	}
+	
+	@Test
+	public void testSimpleAddOperation() throws Exception {
+
+		//setup
+		Calculator calc = new CalculatorImpl();
+		
+		//execute
+		calc.push(2.0);
+		calc.push(3);
+		double result = calc.perform(Operation.add);
+
+		//verify
+		assertEquals(5, result, 0);
+		
+
+	}
+	
+	@Test
+	public void testSimpleMulOperation() throws Exception {
+
+		Calculator calc = new CalculatorImpl();
+		calc.push(2.0);
+		calc.push(3);
+		double result = calc.perform(Operation.mul);
+
+		assertEquals(6, result, 0);
+
+	}
+	
+	@Test
+	public void testSimpleDivOperation() throws Exception {
+
+		Calculator calc = new CalculatorImpl();
+		calc.push(6.0);
+		calc.push(2);
+		double result = calc.perform(Operation.div);
+
+		assertEquals(3, result, 0);
+
+	}
+	
+	
+	
+	
+
+	//
+	@Test(expected = CalculatorException.class)
+	public void testPopOnEmptyStack() throws Exception {
+
+		Calculator calc = new CalculatorImpl();
+		calc.pop();
+
+	}
+
+	@Test
+	public void testDivisionByZero() throws Exception {
+
+		//Setup
+		Calculator calc = new CalculatorImpl();
+		try {
+			calc.push(2);
+			calc.push(0);
+			calc.perform(Operation.div);
+
+			fail("Exception expected");
+			
+
+		} catch (CalculatorException e) {
+			assertEquals("Division by zero", e.getMessage());
+			// e.getCause()
+		}
+
+	}
+}
